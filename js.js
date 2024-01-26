0a4ad5400fc1c9ea7e119ab99341949215214393
Prabin Singh <80961748+prabinsingh99@users.noreply.github.com>
2024-01-26 20:20:57 +0545

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 const endTime = "HAPPY DEEWALI 22 JANUARY 2025"
 document.getElementById("eventTime").innerText = endTime;
 
+//event end date...
 const edate = "22 January 2025 00:00:00"
 
+// Making array of input boxes
 const input = document.getElementsByClassName("inputboxes")
 
-/**
- * Updates the input boxes with the remaining time in days, hours, minutes, and seconds.
- */
+ // Updates the input boxes with the remaining time in days, hours, minutes, and seconds.
 function clock() {
     const end = new Date(edate);
     const start = new Date();
@@ -21,6 +21,7 @@ function clock() {
     input[3].value= Math.floor(timeInSeconds% 60);
 }
 
+// refresh function every second
 setInterval(
     () =>{
         clock()},
